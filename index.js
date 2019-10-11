30ccbae2b9f346f921fe438236b3a286bfddd4b3
Lukas Thoma <lukas@propain-bikes.com>
2019-10-11 10:17:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(8080);
+
+let count = 0;
+let $ipsConnected = [];
+
+io.on('connection', function (socket) {
+  const $ipAddress = socket.handshake.address;
+
+  if (!$ipsConnected.hasOwnProperty($ipAddress)) {
+    $ipsConnected[$ipAddress] = 1;
+    count++;
+
+    socket.emit('counter', {
+      count: count
+    });
+  }
+
+  socket.on('disconnect', function () {
+    if ($ipsConnected.hasOwnProperty($ipAddress)) {
+      delete $ipsConnected[$ipAddress];
+      count--;
+
+      socket.emit('counter', {
+        count: count
+      });
+    }
+  });
+});
+
+app.get('/', function (req, res) {
+  res.json({count: count});
+});
